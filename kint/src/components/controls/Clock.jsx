3c9b16854e742902c2c09a84fbf9ce582be2834d
y-arimura1222 <y.arimura.carecon@gmail.com>
2021-08-26 23:44:19 +0900

    ディレクトリ構成の変更

--- kint/src/components/controls/Clock.jsx
+++ kint/src/components/controls/Clock.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core'
-import { TimeData } from '../TimeData';
+import { TimeData } from '../data/TimeData';
 
 const useStyles = makeStyles(theme => ({
   container: {
