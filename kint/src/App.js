cfe5dfc0f2f0366ee0b55dd80e6648266a22fd9d
y-arimura1222 <y.arimura.carecon@gmail.com>
2021-08-26 23:44:01 +0900

    出退勤入力フォームを移植

--- kint/src/App.js
+++ kint/src/App.js
@@ -1,43 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Clock } from './components/controls/Clock';
 import { ActionButton } from './components/ui/ActionButton';
-import { TimeTable } from './components/controls/TimeTable';
-import { makeStyles } from '@material-ui/core/styles';
 import { ResetButton } from './components/controls/ResetButton';
 
-const useStyles = makeStyles((theme) => ({
-  formContainer: {
-    width: '50%',
-    display: 'flex',
-    flexWrap: 'wrap',
-    margin: '0 auto',
-    justifyContent: 'space-around',
-    marginBottom: theme.spacing(5),
-    '& .MuiTextField-root': {
-      marginLeft: theme.spacing(1),
-      marginRight: theme.spacing(1),
-      width: '200px',
-    }
-  },
-}));
-
 export const App = () => {
-  const classes = useStyles();
-
-  const [startTimeText, setStartTimeText] = useState('10:00')
-  const [finishTimeText, setFinishTimeText] = useState('19:00')
-
-  const onChangeStartTime = (e) => setStartTimeText(`${e.target.value}:00`);
-  const onChangeFinishTime = (e) => setFinishTimeText(`${e.target.value}:00`);
 
   return (
     <>
       <Clock />
-      <form className={classes.formContainer} noValidate>
-        <TimeTable value={ startTimeText } onChange={onChangeStartTime} label="Start time" type="time" defaultValue="10:00" />
-        <TimeTable value={ finishTimeText } onChange={onChangeFinishTime} label="Finish time" type="time" defaultValue="19:00" />
-      </form>
-      <ActionButton startTimeText={ startTimeText } finishTimeText={ finishTimeText } />
+      <ActionButton />
       <ResetButton />
     </>
   )
--- kint/src/components/ui/ActionButton.jsx
+++ kint/src/components/ui/ActionButton.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import { makeStyles, createStyles } from '@material-ui/core/styles';
-import { TimeData } from '../TimeData';
-import { Stamped } from './Stamped';
+import { TimeData } from '../data/TimeData';
+import { TimeTable } from '../controls/TimeTable';
 import { Button } from '../controls/Button';
+import { Stamped } from './Stamped';
 
 const useStyles = makeStyles((theme) =>
   createStyles({
@@ -50,29 +51,51 @@ const useStyles = makeStyles((theme) =>
           fontWeight: 'bold',
         }
       }
-    }
+    },
+    formContainer: {
+      width: '50%',
+      display: 'flex',
+      flexWrap: 'wrap',
+      margin: '0 auto',
+      justifyContent: 'space-around',
+      marginBottom: theme.spacing(5),
+      '& .MuiTextField-root': {
+        marginLeft: theme.spacing(1),
+        marginRight: theme.spacing(1),
+        width: '200px',
+      }
+    },
   })
 );
 
-export const ActionButton = (props) => {
+export const ActionButton = () => {
   // console.log('button');
   // console.log(props);
-  const { startTimeText, finishTimeText } = props;
 
   const classes = useStyles();
 
-  const [isStamp, setIsStamp] = useState(true)
+  // 出退勤時間入力フォームの state
+  const [startTimeText, setStartTimeText] = useState('10:00')
+  const [finishTimeText, setFinishTimeText] = useState('19:00')
+
+  // 各ボタンの state
   const [attendance, setAttendance] = useState(true)
   const [leaving, setLeaving] = useState(true)
   const [behind, setBehind] = useState(true)
   const [leaveEarly, setLeaveEarly] = useState(true)
+
+  // 出退勤時間と現在時刻の差異を見るための state
   const [isStart, setIsStart] = useState(false)
   const [isFinish, setIsFinish] = useState(false)
+
+  // 打刻された際 localStorage の表示を更新するための state
+  const [isStamp, setIsStamp] = useState(true)
 
   const { nowTime } = TimeData();
   const start = startTimeText;
   const finish = finishTimeText;
 
+  // 出退勤時間入力フォームの state と現在時刻の差異を毎秒ごとに監視
   useEffect(() => {
     // 出勤 or 遅刻
     if (!isStart) {
@@ -101,12 +124,16 @@ export const ActionButton = (props) => {
     }
   }, [nowTime])
 
+  // 出退勤時間入力フォームに変更があった際の onChange event
+  const onChangeStartTime = (e) => setStartTimeText(`${e.target.value}:00`);
+  const onChangeFinishTime = (e) => setFinishTimeText(`${e.target.value}:00`);
+
+  // 打刻があった際の onClick event
   const onClickStart = (e) => {
     localStorage.setItem(e.target.innerText, nowTime);
     setIsStamp(!isStamp);
     setIsStart(true);
   }
-
   const onClickFinish = (e) => {
     localStorage.setItem(e.target.innerText, nowTime);
     setIsStamp(!isStamp);
@@ -115,6 +142,10 @@ export const ActionButton = (props) => {
 
   return (
     <div className={classes.container}>
+      <form className={classes.formContainer} noValidate>
+        <TimeTable value={ startTimeText } onChange={onChangeStartTime} label="Start time" type="time" defaultValue="10:00" />
+        <TimeTable value={ finishTimeText } onChange={onChangeFinishTime} label="Finish time" type="time" defaultValue="19:00" />
+      </form>
       <div className={classes.buttonContainer}>
         <Button disabled={attendance} className={classes.Attendance} onClick={ onClickStart }>出勤</Button>
         <Button disabled={leaving} className={classes.Leaving} onClick={ onClickFinish }>退勤</Button>
